3c4fb1f517e423afd39c42a45e6e66057c721ee0
steph-ball <60074804+steph-ball@users.noreply.github.com>
2022-09-25 00:07:54 -0600

    Implemented methods in DFA.java and DFAState.java

--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -3,5 +3,20 @@ package fa.dfa;
 import fa.State;
 
 public class DFAState extends State{
+    private String name;
+    
+    public DFAState(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String toString()
+    {
+        return name;
+    }
 
 }
